bb8bd0aaa2b09e61c7f626e2a4f3a9a67aade835
Dylan Fois <s3786185@student.rmit.edu.au>
2022-08-06 16:33:28 +1000

    Improved registration functionality, added login functionality

--- routes/web/home.js
+++ routes/web/home.js
@@ -6,51 +6,64 @@ const pool = require("../../database/database")
 var router = express.Router()
 
 router.get("/", function(req,res){
-    console.log("Start page")
     res.render("home/index")
 })
 
-// router.get("/home", function(req,res){
-//     console.log("Home page")
-//     res.render("home/home")
-// })
-
-router.get("/home", (req, res) => res.render("home/home"))
+router.get("/home", function(req,res){
+    res.render("home/home")
+})
 
 router.get("/about", function(req,res){
-    console.log("about")
     res.render("home/about")
 })
 
 router.get("/login", function(req,res){
-    console.log("login")
     res.render("home/login")
 })
 
+router.post("/login", async function(req,res){
+
+    results = await pool.query('SELECT passhash FROM users WHERE email = $1', [req.body.email], function(error, results){
+        if (error) {
+            throw error
+          }
+        const passhash = results.rows[0].passhash
+        const password = req.body.password
+
+        match = bcrypt.compare(password, passhash, function(error, match){
+            if (error) {
+                console.log(error)
+                res.status(401).send('error')
+              }else{
+
+            if (match){
+                res.status(201).send('user logged in')
+            }else{
+                res.status(401).send('error')
+            }
+        }
+        })
+    })
+})
+
 router.get("/signup", function(req,res){
-    console.log("sign up")
     res.render("home/signup")
 })
 
 
 router.post("/signup", async function(req,res){
-    console.log(req.body)
-
     const hash = await bcrypt.hash(req.body.password, 10, function(err, hash){
-        console.log(hash)
-        const id = 11
         const firstname = req.body.firstname
         const lastname = req.body.lastname
         const email = req.body.email
         const passhash = hash
-        const salt = "UNDEFINED"
 
-        pool.query('INSERT INTO users (id, firstname, lastname, email, passhash, salt) VALUES ($1, $2, $3, $4, $5, $6)', [id, firstname, lastname, email, passhash, salt], (error, results) => {
+        pool.query('INSERT INTO users (firstname, lastname, email, passhash) VALUES ($1, $2, $3, $4)', [firstname, lastname, email, passhash], (error, results) => {
           if (error) {
-            throw error
+            res.status(401).send('Registration error')
+          }else{
+          res.status(201).send(`Added new user`)
           }
-          console.log("Added user with ID: " + id)
-          res.status(201).send(`User added with ID: ${results.insertId}`)
         })
     })
 
